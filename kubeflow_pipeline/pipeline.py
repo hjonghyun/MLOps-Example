94db6516ba271a6a841c3bfb7fe7409345a8daaf
hjonghyun <coolbeatj@gmail.com>
2021-09-29 06:48:38 +0000

    data test version5

--- kubeflow_pipeline/pipeline.py
+++ kubeflow_pipeline/pipeline.py
@@ -90,7 +90,7 @@ if __name__=="__main__":
     
     pipeline_name = "Mnist"
     pipeline_package_path = "pipeline.zip"
-    version = "v0.2.7"
+    version = "v0.2.8"
 
     experiment_name = "For Develop"
     run_name = "kubeflow study {}".format(version)
