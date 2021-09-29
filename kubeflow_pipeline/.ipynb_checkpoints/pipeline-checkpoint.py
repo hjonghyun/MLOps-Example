deb22f3d01eff14cfa80e8a970eade6277d34655
hjonghyun <coolbeatj@gmail.com>
2021-09-29 08:16:58 +0000

    edit pipeline version

--- kubeflow_pipeline/.ipynb_checkpoints/pipeline-checkpoint.py
+++ kubeflow_pipeline/.ipynb_checkpoints/pipeline-checkpoint.py
@@ -90,7 +90,7 @@ if __name__=="__main__":
     
     pipeline_name = "Mnist"
     pipeline_package_path = "pipeline.zip"
-    version = "v0.2.8"
+    version = "v0.2.9"
 
     experiment_name = "For Develop"
     run_name = "kubeflow study {}".format(version)
